636690c3ae48edee0e16efe2b9a531298cc88239
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2024-03-01 14:30:41 -0500

    Commit current library usage

--- r3/src/main.rs
+++ r3/src/main.rs
@@ -5,20 +5,23 @@ use std::error::{Error};
 #[link(name = "wasminstrument", kind = "static")]
 extern {
     fn instrument_module_buffer(inbuf: *mut libc::c_void, insize: u32, 
-        outsize: *mut u32, routine: *const libc::c_char, args: *const *const libc::c_char, num_args: u32);
+        outsize: *mut u32, routine: *const libc::c_char, args: *const *const libc::c_char, num_args: u32) -> *mut libc::c_void;
 }
 
 
-pub fn loop_count() {
+pub fn loop_count(contents: String) {
     let routine: &str = "loop-count";
     let args: [&str;1] = [""];
     let num_args: u32 = 0;
+    unsafe {
+        instrument_module_buffer(contents.as_ptr(), contents.len(), 
+    }
     println!("Routine: {}", routine);
+    println!("File content: {}", contents);
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string("k.txt")?;
-    println!("File content: {}", contents);
-    loop_count();
+    loop_count(contents);
     return Ok(());
 }
