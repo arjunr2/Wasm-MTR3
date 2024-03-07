8b1f2535779ede0dbc51b6c3393a841a9f7b0e84
Arjun Ramesh <arjunr2@andrew.cmu.edu>
2024-03-07 10:47:38 -0800

    Added CLI args

--- r3/src/main.rs
+++ r3/src/main.rs
@@ -1,13 +1,25 @@
-use libc::{size_t, c_char};
+use libc::{c_char};
+use clap::Parser;
 use std::ffi::CString;
 use std::fs;
 use std::slice;
 use std::error::{Error};
 
+#[derive(Parser,Debug)]
+#[command(version, about, long_about=None)]
+struct CLI {
+    /// Output file name
+    #[arg(short, long)]
+    outfile: String,
+
+    /// Input file name
+    infile: String,
+}
+
 #[link(name = "wasminstrument", kind = "static")]
 extern {
-    fn instrument_module_buffer(inbuf: *const libc::c_char, insize: u32, 
-        outsize: *mut u32, routine: *const libc::c_char, args: *const *const libc::c_char, num_args: u32) -> *mut libc::c_char;
+    fn instrument_module_buffer(inbuf: *const c_char, insize: u32, 
+        outsize: *mut u32, routine: *const c_char, args: *const *const c_char, num_args: u32) -> *mut c_char;
 }
 
 
@@ -23,12 +35,12 @@ pub fn instrument_module(contents: Vec<u8>, routine: &str, args: &[&str]) -> Res
     let outsize_ptr: *mut u32 = &mut outsize;
     let outslice: &[u8];
     unsafe {
-        let outbuf: *mut libc::c_char = 
-            instrument_module_buffer(contents.as_ptr() as *const libc::c_char, 
+        let outbuf: *mut c_char = 
+            instrument_module_buffer(contents.as_ptr() as *const c_char, 
                 contents.len() as u32, 
                 outsize_ptr, 
-                c_routine.as_ptr() as *const libc::c_char, 
-                c_args.as_ptr() as *const *const libc::c_char, 
+                c_routine.as_ptr() as *const c_char, 
+                c_args.as_ptr() as *const *const c_char, 
                 c_args.len() as u32);
         outslice = slice::from_raw_parts(outbuf as *const u8, outsize as usize);
     };
@@ -37,11 +49,12 @@ pub fn instrument_module(contents: Vec<u8>, routine: &str, args: &[&str]) -> Res
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let contents = fs::read("lua.wasm")?;
+    let cli = CLI::parse();
+    let contents = fs::read(cli.infile)?;
     let routine = "memaccess-stochastic";
     let args: [&str;2] = ["30", "1"];
 
     let out_module: &[u8] = instrument_module(contents, routine, &args)?;
-    fs::write("out.wasm", out_module)?;
+    fs::write(cli.outfile, out_module)?;
     return Ok(());
 }
